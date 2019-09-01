0ef6b5f7ed9b69bbaf6b25d5b390f0a8cfa754a9
Yash Jain <yash0307jain@gmail.com>
2019-09-01 08:28:03 +0530

    bootstrap setup done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBERz10j65heLH7JD0UkwAegE-gZqVcWOo',
+    authDomain: 'yashj-organic-shop.firebaseapp.com',
+    databaseURL: 'https://yashj-organic-shop.firebaseio.com',
+    projectId: 'yashj-organic-shop',
+    storageBucket: '',
+    messagingSenderId: '721754931752',
+    appId: '1:721754931752:web:d78802f9d45dab0f'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBERz10j65heLH7JD0UkwAegE-gZqVcWOo",
+    authDomain: "yashj-organic-shop.firebaseapp.com",
+    databaseURL: "https://yashj-organic-shop.firebaseio.com",
+    projectId: "yashj-organic-shop",
+    storageBucket: "",
+    messagingSenderId: "721754931752",
+    appId: "1:721754931752:web:d78802f9d45dab0f"
+  }
 };
 
 /*
